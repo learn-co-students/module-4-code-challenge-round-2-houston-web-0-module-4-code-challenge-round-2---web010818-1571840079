a9d795997bf06dab04f57207a92b5074525212ff
lfrederick2106 <lfrederick2106@gmail.com>
2019-10-23 10:55:39 -0500

    submitting code challenge

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
-const Search = () => {
+const Search = (props) => {
   return (
     <div className="ui huge fluid icon input">
+      <h3>Search</h3>
       <input
         type="text"
         placeholder={"Search your Recent Transactions"}
+        onChange={props.onChange}
       />
       <i className="circular search link icon"></i>
     </div>
--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import Transaction from './Transaction'
 
-const TransactionsList = () => {
+const TransactionsList = (props) => {
+  console.log('transactions:', props.transactions)
 
   return (
     <table className="ui celled striped padded table">
@@ -27,8 +29,10 @@ const TransactionsList = () => {
             </h3>
           </th>
         </tr>
-
-        {"... your code here..."}
+        
+        {props.transactions.map(transaction => {
+          return < Transaction transaction={transaction} key={transaction.id} />
+        })}
 
       </tbody>
     </table>
