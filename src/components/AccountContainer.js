8664e77607d33e8d70b2059239c6dba782c4fb79
Chase Smith <chases832@gmail.com>
2019-10-23 10:53:20 -0500

    good vibes

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -5,25 +5,40 @@ import {transactions} from '../transactionsData'
 
 class AccountContainer extends Component {
 
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
+    this.state={
+      transactions: transactions,
+      search: ''
+    }
+    
 
     // get a default state working with the data imported from TransactionsData
     // use this to get the functionality working
     // then replace the default transactions with a call to the API
 
   }
+  componentDidMount(){
+    fetch(`https://boiling-brook-94902.herokuapp.com/transactions`)
+    .then(r=>r.json())
+    .then(transactions=>this.setState({ transactions: transactions}))
+  }
 
-  handleChange(event) {
+  handleChange = (value) => {
     // your code here
+    this.setState({search: value })
+
+
   }
 
   render() {
+    console.log(transactions)
+    //filter goes here!{this.state.search}gives back what i typed in my search bar.
 
     return (
       <div>
-        <Search />
-        <TransactionsList />
+        <Search  handleChange={this.handleChange}/>
+        <TransactionsList transactions={this.state.transactions} />
       </div>
     )
   }
--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,6 +1,7 @@
 import React from 'react'
+// import AccountContainer from "./AccountContainer"
 
-const TransactionsList = () => {
+const TransactionsList = (props) => {
 
   return (
     <table className="ui celled striped padded table">
@@ -9,6 +10,7 @@ const TransactionsList = () => {
           <th>
             <h3 className="ui center aligned header">
               Posted At
+
             </h3>
           </th>
           <th>
@@ -27,8 +29,30 @@ const TransactionsList = () => {
             </h3>
           </th>
         </tr>
+        {/* 
+        <td>{ props.transactions[0].posted_at }</td>
+        <td> {props.transactions[0].description}</td>
+        <td> {props.transactions[0].category}</td>
+        <td> {props.transactions[0].amount}</td> */}
+        {props.transactions.map(transaction => {
+          return (
+            <tr>
+              <td>{transaction.posted_at}</td>
+              <td >{transaction.description}</td>
+              <td>{transaction.category}</td>
+              <td>{transaction.amount}</td>
+            </tr>
+         )
+        })}
 
-        {"... your code here..."}
+
+
+
+
+
+
+
+
 
       </tbody>
     </table>
