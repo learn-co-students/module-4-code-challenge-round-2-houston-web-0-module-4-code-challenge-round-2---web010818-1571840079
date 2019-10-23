343229be735406cd02e82e763858e012c9e470a6
Jun Liang <illuminatemynight@gmail.com>
2019-10-23 10:48:39 -0500

    first commit

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -8,22 +8,41 @@ class AccountContainer extends Component {
   constructor() {
     super()
 
+    this.state = {
+      transactions: [],
+      results: [],
+      value: ''
+    }
     // get a default state working with the data imported from TransactionsData
     // use this to get the functionality working
     // then replace the default transactions with a call to the API
 
   }
 
-  handleChange(event) {
+  componentDidMount() {
+    fetch('https://boiling-brook-94902.herokuapp.com/transactions')
+      .then(response => response.json())
+      .then(transactions => this.setState({
+        transactions: transactions,
+        results: transactions
+      }))
+  }
+
+  handleChange = value => {
     // your code here
+    this.setState({
+      value: value 
+    })
+    this.setState({
+      results: this.state.transactions.filter( transaction => transaction.description.includes(value) || transaction.category.includes(value))
+    })
   }
 
   render() {
-
     return (
       <div>
-        <Search />
-        <TransactionsList />
+        <Search handleChange={this.handleChange} value={this.state.value}/>
+        <TransactionsList transactions={this.state.results}/>
       </div>
     )
   }
